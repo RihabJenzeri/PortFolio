601b59e05de0bbdb19c3fa2d1c9ca9eb59cda0e9
rihab jenzeri <rihab.jenzeri@gmail.com>
2026-02-21 22:46:13 +0100

    Correction finale du portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/PortFolio/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'), // Point d'entrée explicite
-      },
-    },
-  },
+  server: {
+    port: 5173,
+    open: true
+  }
 })
